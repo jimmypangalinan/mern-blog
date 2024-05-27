5655559cd6b6e416cada398fd9f078eb2b9ec317
Jimmy Pangalinan <jimmi.pangalinan@metrocomjaddi.com>
2024-05-27 23:16:52 +0700

    update

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,12 @@
 import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
-import { Link } from 'react-router-dom'
-import { AiOutlineSearch } from 'react-icons/ai'
-import { FaMoon } from 'react-icons/fa'
+import { Link, useLocation } from 'react-router-dom';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { FaMoon } from 'react-icons/fa';
 
 export default function Header() {
+
+    const path = useLocation().pathname;
+
     return (
         <Navbar className='border-b-2'>
             <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
@@ -31,20 +34,14 @@ export default function Header() {
                 <Navbar.Toggle />
             </div>
             <Navbar.Collapse>
-                <Navbar.Link>
-                    <Link to='/'>
-                        Home
-                    </Link>
+                <Navbar.Link active={path === "/"} as={'div'}>
+                    <Link to='/'>Home</Link>
                 </Navbar.Link>
-                <Navbar.Link>
-                    <Link to='/about'>
-                        About
-                    </Link>
+                <Navbar.Link active={path === "/about"} as={'div'}>
+                    <Link to='/about'>About</Link>
                 </Navbar.Link>
-                <Navbar.Link>
-                    <Link to='/projects'>
-                        Projects
-                    </Link>
+                <Navbar.Link active={path === "/projects"} as={'div'}>
+                    <Link to='/projects'>Projects</Link>
                 </Navbar.Link>
             </Navbar.Collapse>
         </Navbar>
